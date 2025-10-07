db5bf386bdd8aac866f29285e1420adf8d56fad0
Ariena Phannaen <49768823+ArienaTech@users.noreply.github.com>
2025-10-07 18:44:35 +1300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { LanguageProvider } from "@/contexts/LanguageContext";
+import Home from "@/pages/Home";
+import Checkout from "@/pages/Checkout";
+import Profile from "@/pages/Profile";
+import Gems from "@/pages/Gems";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/checkout" component={Checkout} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/gems" component={Gems} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <LanguageProvider>
+          <TooltipProvider>
+            <Toaster />
+            <Router />
+          </TooltipProvider>
+        </LanguageProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
